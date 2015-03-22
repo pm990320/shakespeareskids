9097c0e0cf602a020228ba0cf5e9669469eba2b1
Patrick Menlove <patrickmenlove@hotmail.co.uk>
2015-03-22 14:25:44 +0000

    few more corrections

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
-<form role="search" method="get" class="form-inline" action="<?php echo home_url( '/' ); ?>" id="searchform">
+<form role="search" method="get" class="form-inline col-xs-12" action="<?php echo home_url( '/' ); ?>" id="searchform">
 	<div class="form-group">
 		<div class="col-xs-8">
         <input type="search" style="width:100%" class="form-control search-form" name="s" placeholder="Search..." value="<?php echo get_search_query() ?>"/>
         </div>
-        <div class="col-xs-2">
+        <div class="col-xs-4">
         <button type="submit" style="width:100%" class="btn btn-default search-button">Search</button>
     	</div>
     </div>

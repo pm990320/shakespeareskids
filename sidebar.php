ac94d12768b1f905515e5046491a7f478ddeac1b
Patrick Menlove <patrickmenlove@hotmail.co.uk>
2015-03-22 14:21:25 +0000

    ...

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,9 @@
 <div class="container-fluid">
 	<div class="col-xs-12">
 		<div class="row">
+			<div class="col-xs-12">
 			<a class="btn btn-register" href="<?php echo get_permalink( get_page_by_title( 'Register' ) ) ?>">Register Today</a>
+			</div>
 		</div>
 
 		<div class="row">
@@ -9,15 +11,21 @@
 		</div>
 
 		<div class="row">
+			<div class="col-xs-12">
 			<h4>Our latest Facebook activity:</h4>
+			</div>
 		</div>
 
 		<div class="row">
-			<div class="fb-activity" data-app-id="786238138137417" data-site="developers.facebook.com" data-action="likes, recommends" data-width="100%" data-height="100%" data-colorscheme="light" data-header="true"></div>
+			<div class="col-xs-12">
+				<div class="fb-activity" data-app-id="786238138137417" data-site="developers.facebook.com" data-action="likes, recommends" data-width="100%" data-height="100%" data-colorscheme="light" data-header="true"></div>
+			</div>
 		</div>
 
 		<div class="row" style="margin-top:5%">
-			<div class="fb-like" data-href="https://www.facebook.com/ShakespearesKids" data-width="10vh" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+			<div class="col-xs-12">
+				<div class="fb-like" data-href="https://www.facebook.com/ShakespearesKids" data-width="10vh" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+			</div>
 		</div>
 	</div>
 </div>

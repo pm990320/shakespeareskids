7675a6c6ecf058a8984bccabeed6881618102282
Patrick Menlove <patrickmenlove@hotmail.co.uk>
2015-03-22 14:16:03 +0000

    search form formatting

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
 <form role="search" method="get" class="form-inline" action="<?php echo home_url( '/' ); ?>" id="searchform">
 	<div class="form-group">
+		<div class="col-xs-10">
         <input type="search" class="form-control search-form" name="s" placeholder="Search..." value="<?php echo get_search_query() ?>"/>
+        </div>
+        <div class="col-xs-2">
         <button type="submit" class="btn btn-default search-button">Search</button>
+    	</div>
     </div>
 </form>
